73aa5d260c91f27f80133e2198f9a435f3a6fb08
heathercodes <heather@looka.com>
2019-11-14 19:08:43 -0500

    fix provinces for search

--- src/data/states-provinces.js
+++ src/data/states-provinces.js
@@ -1,5 +1,5 @@
 const states = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
 
-const provinces = ['NL', 'NB', 'NS', 'PEI', 'QC', 'ON', 'MB', 'SK', 'AB', 'BC', 'YK', 'NU', 'NT'];
+const provinces = ['Newfoundland and Labrador', 'New Brunswick', 'Nova Scotia', 'Prince Edward Island', 'Quebec', 'Ontario', 'Manitoba', 'Saskatchewan', 'Alberta', 'British Columbia', 'Yukon', 'Nunavut', 'Northwest Territories'];
 
 export { states, provinces };
