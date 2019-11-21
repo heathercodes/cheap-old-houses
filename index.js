18799c64ad961e3e03c6b8165e772d3e9d1f391e
heathercodes <heather@looka.com>
2019-11-21 17:54:36 -0500

    add koa router and health route

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
-const { ApolloServer, gql } = require('apollo-server-koa'); 
+const { ApolloServer, gql } = require('apollo-server-koa');
 const bodyParser = require('koa-bodyparser');
 const cors = require('@koa/cors');
 const Koa = require('koa');
+const { router } = require('./routes/health')
 
 const port = 9000;
 
 const app = new Koa();
 app.use(cors(), bodyParser());
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 const typeDefs = gql(fs.readFileSync('./schema.graphql', { encoding: 'utf8' }));
 const resolvers = require('./resolvers');
