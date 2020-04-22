dc1355e14bc12b5072b6ce162a6d7b470fc2de15
heathercodes <heather@looka.com>
2020-04-21 22:03:18 -0400

    express conversion because resources are better

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,17 @@
-import { ApolloServer } from 'apollo-server-koa';
-import bodyParser from 'koa-bodyparser';
-import cors from '@koa/cors';
-import Koa from 'koa';
+import { ApolloServer } from 'apollo-server-express';
+import bodyParser from 'body-parser';
+import express, { Router } from 'express';
+import path from 'path';
 import resolvers from './resolvers';
 import schema from './schema';
 
+const router = Router();
 const PORT = 9000;
-const app = new Koa();
-app.use(cors(), bodyParser());
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../client")));
+app.use(router);
 
 const apolloServer = new ApolloServer({ typeDefs: schema, resolvers });
 apolloServer.applyMiddleware({ app, path: '/graphql' });
