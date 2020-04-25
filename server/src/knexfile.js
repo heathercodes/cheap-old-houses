34622026fc4a03870d9b1749fff581036465345f
heathercodes <heather@looka.com>
2020-04-25 19:03:46 -0400

    debugging changes

--- server/src/knexfile.js
+++ server/src/knexfile.js
@@ -15,7 +15,14 @@ module.exports = {
 
     production: {
         client: "pg",
-        connection: process.env.DB_URL_PROD,
+        debug: true,
+        connection: {
+            host: process.env.DATABASE_HOST,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            port: Number(process.env.DATABASE_PORT),
+        },
         migrations: {
             directory: `${__dirname}/db/migrations`
         },
